aac1f4f311ba1cb8dc36e6dc262f4859de7f6405
Zachary S <zasample18+github@gmail.com>
2020-12-23 14:14:42 -0600

    Day 16 Part 2

--- day_16/main.hs
+++ day_16/main.hs
@@ -3,6 +3,7 @@ import System.IO
 import qualified Data.List as List
 import Data.Char
 import qualified Data.Map as Map
+import Data.Maybe (isJust, maybeToList)
 
 -- https://stackoverflow.com/a/4981265/5142683
 splitBy :: (a -> Bool) -> [a] -> [[a]]
@@ -17,16 +18,49 @@ type FieldCheck = Int -> Bool
 orFuncs :: (a -> Bool) -> (a -> Bool) -> a -> Bool
 orFuncs f g x = (f x) || (g x)
 
-helper :: [Int] -> FieldCheck
-helper [s,e] = (\i -> i >= s && i <= e)
+helper1 :: [Int] -> FieldCheck
+helper1 [s,e] = (\i -> i >= s && i <= e)
 
 makeFieldCheck :: [String] -> FieldCheck
 makeFieldCheck wrds =
 	let rangesStrs = filter (all $ orFuncs Data.Char.isDigit (=='-')) wrds in
-		foldl orFuncs (const False) $ map (helper . (map read)) $ map (splitBy (=='-')) rangesStrs
+		foldl orFuncs (const False) $ map (helper1 . (map read)) $ map (splitBy (=='-')) rangesStrs
 
 
+zipLists :: [[a]] -> [[a]]
+zipLists [] = []
+zipLists (l:ls) =
+	case zipLists ls of
+		[] -> map (\i -> [i]) l
+		zs -> zipWith (:) l zs
 
+setDefault :: a -> Int -> a -> [a] -> [a]
+setDefault d 0 v (_:vs) = v:vs
+setDefault d 0 v [] = [v]
+setDefault d i v [] = replicate (i-1) d ++ [v]
+setDefault d i v (vv:vs) = vv : setDefault d (i-1) v vs
+
+
+helper2 :: Int -> [[Int]] -> [[Int]]
+helper2 current possibleFields =
+	case filter (\i -> isJust (List.elemIndex current $ possibleFields !! i)) [0..length possibleFields - 1] of
+		[] -> (
+			case all ((==1) . length) possibleFields of
+				True -> possibleFields
+				False -> helper2 0 possibleFields
+			)
+		[i] -> helper3 current $ setDefault [] i [current] $ map (List.\\ [current]) possibleFields
+		_ -> helper3 current possibleFields
+
+helper3 :: Int -> [[Int]] -> [[Int]]
+helper3 current possibleFields =
+	case filter (\i -> [current] == possibleFields !! i) [0..length possibleFields - 1] of
+		[i] -> helper2 (current+1) $ setDefault [] i [current] $ map (List.\\ [current]) possibleFields
+		_ -> helper2 (current+1) possibleFields
+
+simplifyPossibleFields :: [[Int]] -> [[Int]]
+simplifyPossibleFields possibleFields = helper2 0 possibleFields
+	
 
 main :: IO ()
 main = do
@@ -36,11 +70,10 @@ main = do
 
 	contents <- hGetContents file
 	let [fieldsStrs,_:myticketStrs,_:nearbyStrs] = splitBy (=="") $ lines contents
---	print fieldsStrs
---	print myticketStrs
---	print nearbyStrs
 
 	let fcs = map (makeFieldCheck . words) fieldsStrs
+	let fieldNames = map (head . (splitBy (==':'))) fieldsStrs
+--	print fieldNames
 	let anyField = foldl orFuncs (const False) fcs
 
 	let nearbys = map (\s -> map read $ splitBy (==',') s) nearbyStrs :: [[Int]]
@@ -52,4 +85,20 @@ main = do
 	print $ sum invalidValues
 
 
---	let nearbys' = filter (all anyField) nearbys
+	let nearbys' = filter (all anyField) nearbys
+
+	let fields = zipLists nearbys'
+
+	let fieldIndices = [0..(length fields-1)]
+
+	let possibleFields = map (\values -> filter (\i -> (all (fcs !! i)) values) fieldIndices) fields
+	let solution = concat $ helper2 0 possibleFields
+	-- corresponding location in this list contains the index of the fieldName for that index
+	-- e.g. [3,0,2,1] means that field names of "a","b","c","d" and a ticket of 4,5,6,7, d=4, a=5, c=6, b=7
+
+	let departureFields = concatMap (maybeToList . \e -> List.elemIndex e solution) $ filter (\i -> List.isPrefixOf "departure" (fieldNames !! i)) [0..length fieldNames-1]
+	
+	let myTicket = map read $ splitBy (==',') $ head myticketStrs :: [Int]
+
+	print $ foldl (*) 1 $ map (myTicket !!) departureFields
+
