1c7c44c1fbdb7c116e2082bf3b1bf8df1fde39e1
Zachary S <zasample18+github@gmail.com>
2021-06-23 15:47:21 -0500

    Day 1 in Rust

---
+++ rust/day_1/part2.rs
@@ -0,0 +1,34 @@
+use std::io;
+
+fn solve(prices: &Vec<u32>) -> Option<u32> {
+    for i in 0..prices.len() {
+        for j in 0..i {
+            for k in 0..j {
+                if prices[i] + prices[j] + prices[k] == 2020 {
+                    return Some(prices[i] * prices[j] * prices[k])
+                }
+            }
+        }
+    }
+    None
+}
+
+fn main() -> io::Result<()> {
+    let mut line = String::new();
+    let mut prices: Vec<u32> = Vec::new();
+
+    'read: loop {
+        match io::stdin().read_line(&mut line) {
+            Ok(0) => { break 'read; }
+            _ => {
+                match line.trim().parse() {
+                    Ok(price) => { prices.push(price); }
+                    Err(err) => { println!("{}", line); return Err(io::Error::new(io::ErrorKind::InvalidData, err)); }
+                };
+            }
+        }
+        line.clear();
+    }
+    println!("{}", solve(&prices).unwrap());
+    Ok(())
+}
